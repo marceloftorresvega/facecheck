501f0b15e5ad6d5ada3b78ac0b4239afe8caab9b
mtorres <mtorres@kdu.cl>
2019-11-13 12:25:37 -0300

    correccion de frame de ventana

--- src/main/java/org/tensa/facecheck/main/VisLoad.java
+++ src/main/java/org/tensa/facecheck/main/VisLoad.java
@@ -293,7 +293,7 @@ public class VisLoad extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jSplitPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 238, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jSplitPane1)
                 .addContainerGap())
         );
 
