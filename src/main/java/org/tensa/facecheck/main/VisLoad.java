e90c8d7ffbf459f58772dc3225c6994d61f81547
Marcelo Torres <marcelo.torres.vega@gmail.com>
2019-12-23 04:07:51 -0300

    eliminacion de funcion coseno, y reemplazo por random en generacion de pesos, actualizacion de pantalla retorna a lineal

--- src/main/java/org/tensa/facecheck/main/VisLoad.java
+++ src/main/java/org/tensa/facecheck/main/VisLoad.java
@@ -745,14 +745,14 @@ public class VisLoad extends javax.swing.JFrame {
         log.info("iniciando 1.0..<{},{}>",hidStep, inSize);
         weightsH = (DoubleMatriz)new DoubleMatriz(new Dominio(hidStep, inSize)).matrizUno();
         log.info("iniciando 1.1..<{},{}>",hidStep, inSize);
-        weightsH.replaceAll((ParOrdenado i, Double v) -> Math.cos( Math.PI * (i.getColumna() / inSize + i.getFila()/ hidStep) ) );
+        weightsH.replaceAll((ParOrdenado i, Double v) -> 0.5-Math.random() );
 //        log.info("iniciando 1.2..<{},{}>",hidStep, inSize);
 //        weightsH = (DoubleMatriz)weightsH.productoEscalar( hidStep / Math.sqrt(weightsH.distanciaE2().get(Indice.D1)) );
         
         log.info("iniciando 2.0..<{},{}>",outSize, hidStep);
         weightsO = (DoubleMatriz)new DoubleMatriz(new Dominio(outSize, hidStep)).matrizUno();
         log.info("iniciando 2.1..<{},{}>",outSize, hidStep);
-        weightsO.replaceAll((ParOrdenado i, Double v) -> -Math.cos( Math.PI * (i.getColumna() / hidStep + i.getFila()/ outSize) ) );
+        weightsO.replaceAll((ParOrdenado i, Double v) -> 0.5-Math.random() );
 //        log.info("iniciando 2.2..<{},{}>",outSize, hidStep);
 //        weightsO = (DoubleMatriz)weightsO.productoEscalar( hidStep/ Math.sqrt(weightsO.distanciaE2().get(Indice.D1)) );
         
@@ -1073,7 +1073,7 @@ public class VisLoad extends javax.swing.JFrame {
                     Graphics2D localg = (Graphics2D)g;
                     float escala = (float)respuesta.getBounds().width / (float)bufferImageFiltered.getWidth();
                     AffineTransform xforM = AffineTransform.getScaleInstance(escala, escala);
-                    AffineTransformOp rop = new AffineTransformOp(xforM, AffineTransformOp.TYPE_BICUBIC);
+                    AffineTransformOp rop = new AffineTransformOp(xforM, AffineTransformOp.TYPE_BILINEAR);
                     localg.drawImage(bufferImageFiltered, rop, 0     , 0);
                     
                 }
