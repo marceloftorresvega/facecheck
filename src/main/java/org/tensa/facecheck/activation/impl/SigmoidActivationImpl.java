5d76a42e825ed45f7de78819106c7a4301b41450
mtorres <mtorres@kdu.cl>
2020-02-24 18:18:17 -0300

    se crea soporte funciones de activacion multiples

---
+++ src/main/java/org/tensa/facecheck/activation/impl/SigmoidActivationImpl.java
@@ -0,0 +1,61 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2020 Marcelo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.tensa.facecheck.activation.impl;
+
+import java.util.function.Function;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tensa.facecheck.activation.Activation;
+import org.tensa.tensada.matrix.NumericMatriz;
+
+
+public abstract class SigmoidActivationImpl<N extends Number> implements Activation<N> {
+    protected final Logger log = LoggerFactory.getLogger(SigmoidActivationImpl.class);
+    private Function<Double, N> mapper;
+
+    public SigmoidActivationImpl(Function<Double, N> mapper) {
+        this.mapper = mapper;
+    }
+    
+    
+    @Override
+    public Function<NumericMatriz<N>, NumericMatriz<N>> getActivation() {
+        return (m)-> {
+            m.replaceAll((indice,v) -> {
+                return m.inversoMultiplicativo(m.sumaDirecta(
+                        m.getUnoValue(),
+                        getMapper().apply(Math.exp(-v.doubleValue()))
+                        ));
+            });
+            return m;
+        };
+    }
+
+    @Override
+    public Function<Double, N> getMapper() {
+        return mapper;
+    }
+    
+    
+}
