a50476b91c08612c9ea4ae23339589ae36000217
mtorres <mtorres@kdu.cl>
2020-02-25 17:58:09 -0300

    se formaliza escalamiento de salida de inputLayer

---
+++ src/main/java/org/tensa/facecheck/layer/impl/OutputScale.java
@@ -0,0 +1,68 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2020 Marcelo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.tensa.facecheck.layer.impl;
+
+import java.util.function.Function;
+import org.tensa.tensada.matrix.Indice;
+import org.tensa.tensada.matrix.NumericMatriz;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class OutputScale {
+
+    private OutputScale() {
+    }
+    
+    public static <N extends Number> NumericMatriz<N> sameEscale(NumericMatriz<N> matriz, Function<Double, N> mapper) {
+        return matriz;
+    }
+    
+    public static <N extends Number> NumericMatriz<N> prevent01(NumericMatriz<N> matriz, Function<Double, N> mapper) {
+        N escala = mapper.apply(254.0 / 255.0);
+        NumericMatriz<N> margen = matriz.matrizUno().productoEscalar(mapper.apply(0.5));
+        matriz.productoEscalar(escala).adicion(margen);
+        return matriz;
+    }
+    
+    public static <N extends Number> NumericMatriz<N> scale(NumericMatriz<N> matriz, Function<Double, N> mapper) {
+        N escala = mapper.apply(1 / 255.0);
+        matriz.productoEscalar(escala);
+        return matriz;
+    }
+    
+     public static <N extends Number> NumericMatriz<N> normalized(NumericMatriz<N> matriz, Function<Double, N> mapper) {
+        NumericMatriz<N> d = matriz.distanciaE2();
+        N normalizador = matriz.inversoMultiplicativo(mapper.apply(Math.sqrt(d.get(Indice.D1).doubleValue())));
+        return matriz.productoEscalar(normalizador);
+     }
+    
+     public static <N extends Number> NumericMatriz<N> reflectance(NumericMatriz<N> matriz, Function<Double, N> mapper) {
+            NumericMatriz<N> r = matriz.productoPunto(matriz.matrizUno());
+            N reflector = matriz.inversoMultiplicativo(r.get(Indice.D1));
+            return matriz.productoEscalar(reflector);
+     }
+     
+}
