7760c480708a3d756e69113318e6f87fc3bcbd58
mtorres <mtorres@kdu.cl>
2020-05-18 17:50:29 -0400

    extension a funciones de activación: REctificacion lineal tangente hiperbolica

---
+++ src/main/java/org/tensa/facecheck/activation/impl/LearningTanHyperActivationImpl.java
@@ -0,0 +1,56 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2020 Marcelo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.tensa.facecheck.activation.impl;
+
+import java.io.IOException;
+import java.util.function.BiFunction;
+import org.tensa.tensada.matrix.NumericMatriz;
+
+
+public class LearningTanHyperActivationImpl<N extends Number> extends TanHyperActivationImpl<N> {
+
+    @Override
+    public BiFunction<NumericMatriz<N>, NumericMatriz<N>, NumericMatriz<N>> getError() {
+        return  (learning, output) -> {
+            NumericMatriz<N> m1 = output.matrizUno();
+            try (
+                NumericMatriz<N> diferencia = learning.substraccion(output);
+                NumericMatriz<N> semiSuma = m1.adicion(output);
+                NumericMatriz<N> semiResta = m1.substraccion(output);
+                    ) {
+                
+                m1.replaceAll((indice,v) -> 
+                        m1.productoDirecto(diferencia.get(indice),
+                            m1.productoDirecto(semiSuma.get(indice), semiResta.get(indice))));
+                
+                
+            } catch( IOException ex ) {
+                //
+            }
+            return m1;
+            
+        };
+    }
+    
+}
