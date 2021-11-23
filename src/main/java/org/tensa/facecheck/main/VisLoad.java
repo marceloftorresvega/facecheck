a236762fa73cd55d02f64888c38f1d198cee9800
Marcelo Torres <mtorres@kdu.cl>
2021-11-23 00:59:35 -0300

    suprime unchecked warnings

--- src/main/java/org/tensa/facecheck/main/VisLoad.java
+++ src/main/java/org/tensa/facecheck/main/VisLoad.java
@@ -843,6 +843,7 @@ public class VisLoad extends javax.swing.JFrame {
         });
     }//GEN-LAST:event_enmascaraResultadoActionPerformed
 
+    @SuppressWarnings("unchecked")
     private void procesarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_procesarActionPerformed
 
         procesar.setEnabled(false);
@@ -964,6 +965,7 @@ public class VisLoad extends javax.swing.JFrame {
         }).start();
     }//GEN-LAST:event_procesarActionPerformed
 
+    @SuppressWarnings("unchecked")
     private void cleanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cleanActionPerformed
         log.info("iniciando 0...");
         int inStep = (int) inNeurs.getValue();
@@ -1785,6 +1787,7 @@ public class VisLoad extends javax.swing.JFrame {
 
         private final javax.swing.JComboBox<WeightModelingEnum> style;
 
+    @SuppressWarnings("unchecked")
         public NeuronStyleTableCellEditorImpl() {
             this.style = new javax.swing.JComboBox<>(getModelingComboBoxModel());
             style.setSelectedIndex(0);
@@ -1812,6 +1815,7 @@ public class VisLoad extends javax.swing.JFrame {
             return new DefaultComboBoxModel<>(WeightCreationEnum.values());
         }
 
+    @SuppressWarnings("unchecked")
         public CreationWeightTableCellEditorImpl() {
             creation = new JComboBox<>(getCreationComboBoxModel());
             creation.setSelectedIndex(0);
@@ -1841,6 +1845,7 @@ public class VisLoad extends javax.swing.JFrame {
             return new DefaultComboBoxModel<>(ActivationFunctionEnum.values());
         }
 
+        @SuppressWarnings("unchecked")
         public ActivationFunctionTableCellEditorImpl() {
             activation = new JComboBox<>(getActivationComboBoxModel());
             activation.setSelectedIndex(0);
@@ -1872,6 +1877,7 @@ public class VisLoad extends javax.swing.JFrame {
 
         private final javax.swing.JComboBox<ActivationFunctionEnum> strategy;
 
+        @SuppressWarnings("unchecked")
         public LearningEstrategyTableCellEditorImpl() {
             this.strategy = new JComboBox<>(getLearninEstrategyComboBoxModel());
             this.strategy.setSelectedIndex(0);
