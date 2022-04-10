ac135a82a207fade649c30e4a9ddf16e0245bceb
Marcelo Torres Vega <mtorres@kdu.cl>
2022-04-10 01:26:31 -0400

    nueva activacon normal, nuava LeakyRelu activacion

---
+++ src/main/java/org/tensa/facecheck/activation/impl/LeakyReluActivationImpl.java
@@ -0,0 +1,79 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2020 Marcelo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.tensa.facecheck.activation.impl;
+
+import org.tensa.facecheck.activation.utils.ActivationUtils;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import org.tensa.facecheck.activation.Activation;
+import org.tensa.tensada.matrix.NumericMatriz;
+
+/**
+ * Implementacion funcion de activacion para regresion lineal y uso de derivada
+ * para calculo de error, optimizada para el uso de la salida de la capa
+ *
+ * @author Marcelo
+ * @param <N>
+ */
+public class LeakyReluActivationImpl<N extends Number> implements Activation<N> {
+
+    protected final N gain;
+
+    public LeakyReluActivationImpl(N gain) {
+        this.gain = gain;
+    }
+
+    @Override
+    public Function<NumericMatriz<N>, NumericMatriz<N>> getActivation() {
+        return (m) -> {
+            return m.entrySet().stream()
+                    .collect(ActivationUtils.entryToMatriz(m,
+                            e -> {
+                                if (e.getValue().doubleValue() > 0.0) {
+                                    return e.getValue();
+                                } else {
+                                    return m.productoDirecto(gain, e.getValue());
+                                }
+                            }));
+        };
+    }
+
+    @Override
+    public BiFunction<NumericMatriz<N>, NumericMatriz<N>, NumericMatriz<N>> getError() {
+        return (NumericMatriz<N> learning, NumericMatriz<N> output) -> {
+
+            return output.entrySet().stream()
+                    .collect(ActivationUtils.entryToMatriz(output,
+                            (e) -> output.restaDirecta(
+                                    learning.get(e.getKey()),
+                                    e.getValue())));
+        };
+    }
+
+    @Override
+    public boolean isOptimized() {
+        return true;
+    }
+
+}
---
+++ src/main/java/org/tensa/facecheck/activation/impl/NormalActivationImpl.java
@@ -0,0 +1,106 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2021 Marcelo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.tensa.facecheck.activation.impl;
+
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+import org.tensa.facecheck.activation.Activation;
+import org.tensa.facecheck.activation.utils.ActivationUtils;
+import org.tensa.tensada.matrix.NumericMatriz;
+
+/**
+ * retorna la matriz normal para cada columna de la matriz de ingreso
+ *
+ * funcion de error de x/ (x2+c): ((x2+c) - 2x2) / (x2 + c)2 -> c-x2 / (x2+c)2
+ *
+ * @author Marcelo
+ * @param <N> clase de nummero utilizado
+ */
+public class NormalActivationImpl<N extends Number> implements Activation<N> {
+
+    @Override
+    public Function<NumericMatriz<N>, NumericMatriz<N>> getActivation() {
+        return this::getSoftMax;
+    }
+
+    private NumericMatriz<N> getSoftMax(NumericMatriz<N> m) {
+
+        Map<Integer, N> sumas2 = m.entrySet().stream()
+                .collect(Collectors.toMap(
+                        e -> e.getKey().getColumna(),
+                        e -> m.productoDirecto(e.getValue(), e.getValue()),
+                        m::sumaDirecta));
+
+        return m.entrySet().stream()
+                .collect(ActivationUtils.entryToMatriz(
+                        m,
+                        e -> m.productoDirecto(
+                                e.getValue(),
+                                m.inversoMultiplicativo(
+                                        sumas2.get(e.getKey().getColumna())
+                                ))
+                ));
+    }
+
+    @Override
+    public BiFunction<NumericMatriz<N>, NumericMatriz<N>, NumericMatriz<N>> getError() {
+        return (leraning, neta) -> {
+
+            UnaryOperator<N> cuad = v -> neta.productoDirecto(v, v);
+
+            NumericMatriz<N> cuadrados = neta.entrySet().stream()
+                    .collect(ActivationUtils.entryToMatriz(
+                            neta,
+                            cuad.compose(Map.Entry::getValue)));
+
+            Map<Integer, N> sumas2 = cuadrados.entrySet().stream()
+                    .collect(Collectors.toMap(
+                            e -> e.getKey().getColumna(),
+                            Map.Entry::getValue,
+                            neta::sumaDirecta));
+
+            return cuadrados.entrySet().stream()
+                    .collect(ActivationUtils.entryToMatriz(
+                            neta,
+                            e -> neta.productoDirecto(
+                                    neta.restaDirecta(
+                                            sumas2.get(e.getKey().getColumna()),
+                                            neta.productoDirecto(
+                                                    neta.mapper(2.0),
+                                                    cuadrados.get(e.getKey()))),
+                                    cuad.andThen(neta::inversoMultiplicativo)
+                                            .compose(sumas2::get)
+                                            .apply(e.getKey().getColumna()))));
+        };
+    }
+
+    @Override
+    public boolean isOptimized() {
+        return false;
+    }
+
+}
